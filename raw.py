0233bd80fe774e14ab51ab4e230a773a1bb62cd5
Mahesh Salpure <113253278+themaheshsalpure@users.noreply.github.com>
2024-02-07 01:20:52 +0530

    Add files via upload

---
+++ raw.py
@@ -0,0 +1,9 @@
+my_tuple = (3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5)
+
+print(my_tuple)
+# Output: (3, 1, 4, 5, 9, 2, 6, 5, 3, 5)
+
+# Iterate over the tuple
+for element in my_tuple:
+    print(element)
+# Output will be in the order of elements in the tuple
